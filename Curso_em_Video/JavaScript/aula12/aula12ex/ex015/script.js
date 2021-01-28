28eea9e59417beefff54618dc28d7cda15eeab20
Bruno Paulela <73469376+paulelabruno@users.noreply.github.com>
2021-01-27 23:30:08 -0300

    atualização do curso

--- Curso_em_Video/JavaScript/aula12/aula12ex/ex015/script.js
+++ Curso_em_Video/JavaScript/aula12/aula12ex/ex015/script.js
@@ -9,11 +9,49 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked){
             gênero = `Homem`
+            if (idade >= 0 && idade < 12){
+                //criança
+                gênero = 'um menino'
+                img.setAttribute('src', 'h_cri.png')
+            } else if (idade <= 18) {
+                //adolescente
+                gênero = 'um adolescente'
+                img.setAttribute('src', 'h_adol.png')
+            } else if (idade > 18 && idade < 55){
+                //adulto
+                gênero = 'um homem'
+                img.setAttribute('src', 'h_adul.png')
+            } else if (idade >= 55){
+                //idoso
+                gênero = 'um senhor'
+                img.setAttribute('src', 'h_sen.png')
+            }
         } else if (fsex[1].checked) {
             gênero = `Mulher`
+            if (idade >= 0 && idade < 12){
+                //criança
+                gênero = 'uma menina'
+                img.setAttribute('src', 'm_cri.png')
+            } else if (idade <= 18) {
+                //adolescente
+                gênero = 'uma adolescente'
+                img.setAttribute('src', 'm_adol.png')
+            } else if (idade > 18 && idade < 55){
+                //adulto
+                gênero = 'uma mulher'
+                img.setAttribute('src', 'm_adul.png')
+            } else if (idade >= 55){
+                //idoso
+                gênero = 'uma senhora'
+                img.setAttribute('src', 'm_sen.png')
+            }
         }
-        res.innerHTML = `Detectamos ${gênero} com ${idade} anos`
+        res.style.TextAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos de idade`
+        res.appendChild(img)
     }
 }
---
+++ Curso_em_Video/JavaScript/aula13/ambiente.js
@@ -0,0 +1,17 @@
+var c = 1
+while (c <= 1) {
+    console.log(`passo ${1}`)
+    c++
+}
+
+
+
+/*
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+console.log(`Tudo bem?`)
+*/
